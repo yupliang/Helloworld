6a2faffbcd0935643947d3e227a503997d1d2746
app-01 <yupeiliang110@163.com>
2015-10-14 22:53:55 +0800

    完成了教程

---
+++ Helloworld/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  Helloworld
+//
+//  Created by app-01 on 15/10/14.
+//  Copyright © 2015年 __orgnazation__. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+@end
---
+++ Helloworld/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  Helloworld
+//
+//  Created by app-01 on 15/10/14.
+//  Copyright © 2015年 __orgnazation__. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
